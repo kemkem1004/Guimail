3f3bb85d376ba0dc343c42c49349dba02610e43a
kemkem1004 <152306897+kemkem1004@users.noreply.github.com>
2026-03-10 11:09:47 +0700

    Create MailServer.java for mail functionalities

---
+++ src/MailServer.java
@@ -0,0 +1,17 @@
+// MailServer.java
+
+public class MailServer {
+    // This class will handle mail server functionalities
+
+    public void sendEmail(String recipient, String subject, String message) {
+        // Implement method to send email
+    }
+
+    public void receiveEmail() {
+        // Implement method to receive email
+    }
+
+    public static void main(String[] args) {
+        // Main method for testing purposes
+    }
+}
